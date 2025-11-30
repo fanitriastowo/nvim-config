8848808f397f5cb21bfe539e0e648dad038a3b6c
Fani Triastowo <fani.triastowo@gmail.com>
2025-11-30 22:07:03 +0700

    clipboard: fix selection paste

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,23 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
----@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "chadracula-evondev",
+  theme = "decay",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+M.general = {
+  v = {
+    -- Paste over selection without overriden clipboard
+    ["p"] = { '"_dP', "Paste without replacing clipboard" },
+  },
 }
 
 return M
