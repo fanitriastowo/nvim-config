4fa09dd748de029cf7a8848e973dca5529d41831
Fani Triastowo <fani.triastowo@gmail.com>
2025-12-02 11:33:02 +0700

    shell: dynamic shell config for different OS

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,8 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
+local is_win = vim.loop.os_uname().sysname == "Windows_NT"
+
 o.cursorlineopt ='both' -- to enable cursorline!
 o.breakindent = true
 o.showbreak = string.rep(" ", 3)
@@ -11,3 +13,17 @@ o.scrolloff = 10
 o.smartindent = true
 o.colorcolumn = "110"
 o.textwidth = 110
+
+if is_win then
+  if vim.fn.executable("pwsh") == 1 then
+    o.shell = "pwsh"
+  else
+    o.shell = "powershell"
+  end
+else
+  if vim.fn.executable("zsh") == 1 then
+    o.shell = "zsh"
+  else
+    o.shell = "bash"
+  end
+end
