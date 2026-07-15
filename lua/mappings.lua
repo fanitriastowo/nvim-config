3c751c8b20952702b6af1c5d3a02a1508dbbaa04
Fani Triastowo <fani.triastowo@gmail.com>
2026-07-15 19:17:02 +0700

    mappings: add a new horizontal line shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,13 +3,13 @@ require "nvchad.mappings"
 -- add yours here
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", {expr = true, silent = true})
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", {expr = true, silent = true})
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
 vim.keymap.set("n", "<leader>cl", function()
-  local path = vim.fn.expand("%:.")
-  local line = vim.fn.line(".")
+  local path = vim.fn.expand "%:."
+  local line = vim.fn.line "."
   vim.fn.setreg("+", path .. "#L" .. line)
   print("Copied: " .. path .. "#L" .. line)
 end, { desc = "Copy file path and line number" })
@@ -37,10 +37,19 @@ vim.keymap.set("n", "zL", "zL", { desc = "Scroll right half screen" })
 vim.keymap.set("n", "zH", "zH", { desc = "Scroll left half screen" })
 
 vim.keymap.set("n", "<leader>>", function()
-  require("nvim-tree.api").tree.resize({ relative = 5 })
+  require("nvim-tree.api").tree.resize { relative = 5 }
 end, { desc = "Widen nvim-tree" })
 
 vim.keymap.set("n", "<leader><", function()
-  require("nvim-tree.api").tree.resize({ relative = -5 })
+  require("nvim-tree.api").tree.resize { relative = -5 }
 end, { desc = "Narrow nvim-tree" })
 
+-- a horizontal line
+vim.keymap.set("n", "<leader>=", function()
+  local line = vim.api.nvim_get_current_line()
+  local count = 80 - #line
+  if count < 1 then
+    count = 80
+  end
+  vim.api.nvim_put({ string.rep("=", count) }, "c", true, true)
+end, { desc = "Create = line" })
